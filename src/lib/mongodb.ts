980f78871bb596eeb55dbd17c812e15d277ea139
the3rdeye99 <oluwatobi0112@gmail.com>
2025-09-22 18:58:49 +0100

    Update mongodb.ts to point to .env

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 // Define the shape of our cached mongoose global
 interface MongooseCache {
@@ -6,45 +6,43 @@ interface MongooseCache {
   promise: Promise<typeof mongoose> | null;
 }
 
-// Declare the global mongoose cache
+// Extend global type to avoid redeclaration errors
 declare global {
-  var mongoose: MongooseCache;
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseCache | undefined;
 }
 
-const MONGODB_URI = "mongodb+srv://kfc-school-bills:kayodefilani_123@cluster0.u3ihnqh.mongodb.net/school-bills?retryWrites=true&w=majority&appName=Cluster0";
+// Load MongoDB URI from environment
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("❌ Please add your MongoDB URI to .env");
+}
 
 // Initialize the global mongoose cache if it doesn't exist
 if (!global.mongoose) {
-  global.mongoose = {
-    conn: null,
-    promise: null
-  };
+  global.mongoose = { conn: null, promise: null };
 }
 
 async function connectToDatabase(): Promise<typeof mongoose> {
-  // If we have a connection, return it
-  if (global.mongoose.conn) {
+  if (global.mongoose?.conn) {
     return global.mongoose.conn;
   }
 
-  // If we don't have a promise to connect, create one
-  if (!global.mongoose.promise) {
-    const opts = {
-      bufferCommands: true,
-    };
-
-    global.mongoose.promise = mongoose.connect(MONGODB_URI!, opts);
+  if (!global.mongoose?.promise) {
+    const opts = { bufferCommands: true };
+    if (!MONGODB_URI) {
+      throw new Error("❌ Please add your MongoDB URI to .env");
+    }
+    global.mongoose!.promise = mongoose.connect(MONGODB_URI as string, opts);
   }
 
   try {
-    // Wait for the connection
-    const connection = await global.mongoose.promise;
-    global.mongoose.conn = connection;
-    return connection;
-  } catch (e) {
-    // If there's an error, clear the promise and throw
-    global.mongoose.promise = null;
-    throw e;
+    global.mongoose!.conn = await global.mongoose!.promise;
+    return global.mongoose!.conn;
+  } catch (err) {
+    global.mongoose!.promise = null;
+    throw err;
   }
 }
 
